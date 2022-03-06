b8f591a571a14dfce1d10b7d9fb1b02fd49f5ae0
khaleelr4 <khaleelr4@gmail.com>
2022-03-06 17:38:37 +0500

    first commit

---
+++ Facebook_Clone/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "back-image-1": "url('/Images/background.jpg')",
+        "back-image-2": "url('/Images/mobile_background.jpg')"
+
+      }),
+    },
+  },
+  plugins: [],
+}
